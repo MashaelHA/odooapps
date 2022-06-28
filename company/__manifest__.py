e6ed4d8aef9ad8bfa4c79f59fea9054191d5973e
MashaelHA <mashailhalasmari@gmail.com>
2022-06-28 11:16:13 +0300

    [ADD] company

---
+++ company/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Company',
+    'version': '1.1',
+    'summary': "Company Management System",
+    'sequence': 1,
+    'description': "This is company management system software suppored in"
+                   "Odoo v15",
+    'category': 'Company',
+    'website': 'https://freeweblearns.blogspot.com',
+    'depends':['base']
+}
